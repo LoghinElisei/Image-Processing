8e546b8a978a23990af7270a72c7ae4d66fbf98d
LoghinElisei <loghinelisei04@gmail.com>
2025-11-15 14:29:40 +0200

    Nearest neigbor, liniar interpolation with x,y zoom scale algorithm

--- L6/p2/Lab2_PIM.cpp
+++ L6/p2/Lab2_PIM.cpp
@@ -31,46 +31,71 @@ int main(int argc, char *argv[])
 	QString imageDir = "Images/";
 
 	std::cout << "Choose one photo\n\t[1]-CT [2]-Lena [3]-Circle [4]-Harvest \n";
-	int option ;
+	int option;
 	std::cin >> option;
-	char file_name[64]="\0"; 
-	switch(option)
+	char file_name[64] = "\0";
+	switch (option)
 	{
-		case 1:
-			strcpy(file_name,"ct.jpg");
-			break;
-		case 2:
-			strcpy(file_name,"lena512.bmp");
-			break;
-		case 3:
-			strcpy(file_name,"Fig5.03.jpg");
-			break;
-		default:
-			strcpy(file_name,"photo.jpg");
-			break;
+	case 1:
+		strcpy(file_name, "ct.jpg");
+		break;
+	case 2:
+		strcpy(file_name, "lena512.bmp");
+		break;
+	case 3:
+		strcpy(file_name, "Fig5.03.jpg");
+		break;
+	default:
+		strcpy(file_name, "photo.jpg");
+		break;
 	}
 
 	QString imageFile(imageDir + file_name);
 	grid->addImage(imageFile, 0, 0);
 
 	unsigned char *img = Tools::readImageGray8(imageFile, w, h);
-	// int zoom;
+	int zoomX, zoomY;
 	// std::cout<<"zoom = ";
 	// std::cin>>zoom;
-	
+	std::cout << "\t[1]-Dublare imagine [2]-Interpolare liniara\n\t";
+	std::cout << "[3]-Nearest neighbor [4]-Interpolare liniara cu x zoom\n";
+	std::cin >> option;
+
 	unsigned char *zoomed_image;
 
+	switch (option)
+	{
+	case 1:
+		zoomed_image = doubleImageResolution(img, w, h);
+		grid->addImage(zoomed_image, w * 2, h * 2, 0, 2, "double image");
+		break;
+	case 2:
+		zoomed_image = liniarInterpolationZoomImage(img, w, h,2,2);
+		grid->addImage(zoomed_image, w * 2, h * 2, 0, 2, "double image");
+		break;
+	case 3:
+		std::cout << "zoomX = ";
+		std::cin >> zoomX;
+		std::cout << "zoomY = ";
+		std::cin >> zoomY;
+		zoomed_image = nearestNeighborZoomImage(img, w, h,zoomX,zoomY);
+		grid->addImage(zoomed_image, w * zoomX, h * zoomY, 0, 2, "zoomed image");
+		break;
+	case 4:
+		std::cout << "zoomX = ";
+		std::cin >> zoomX;
+		std::cout << "zoomY = ";
+		std::cin >> zoomY;
+		zoomed_image = liniarInterpolationZoomImage(img, w, h,zoomX,zoomY);
+		grid->addImage(zoomed_image, w * zoomX, h * zoomY, 0, 2, "zoomed image");
 
-	zoomed_image = doubleImageResolution(img, w, h);
-	grid->addImage(zoomed_image, w*zoom, h*zoom, 0, 2, "zoomed image");
-
-	
+		break;
+	}
 
 	grid->show();
 	status = a.exec();
 
-
-	delete []zoomed_image;
+	delete[] zoomed_image;
 
 	return status;
 }
--- L6/p2/operatii.h
+++ L6/p2/operatii.h
@@ -8,35 +8,121 @@ unsigned char *doubleImageResolution(unsigned char *img, int w, int h)
 	new_w = w * zoom;
 	unsigned char *g = new unsigned char[new_w * new_h];
 
+	int i = 0;
+	int j = 0;
+	for (int y = 0; y < h; y++)
+	{
+		i = y * zoom;
+		for (int x = 0; x < w; x++)
+		{
+			j = x * zoom;
+			g[i * new_w + j] = img[y * w + x];
+		}
+	}
+
+	for (int y = 0; y < new_h; y += zoom)
+	{
+		for (int x = 0; x < new_w - zoom; x += zoom)
+		{
+			g[y * new_w + (x + 1)] = g[y * new_w + x];
+		}
+	}
+
+	for (int y = 0; y < new_h - zoom; y += zoom)
+	{
+		for (int x = 0; x < new_w; x++)
+		{
+			g[(y + 1) * new_w + x] = g[y * new_w + x];
+		}
+	}
+
+	return g;
+}
+
+unsigned char *liniarInterpolationZoomImage(unsigned char *img, int w, int h, int scaleX, int scaleY)
+{
+
+	int new_w, new_h;
+	new_h = h * scaleY;
+	new_w = w * scaleX;
+	unsigned char *g = new unsigned char[new_w * new_h];
 
 	int i = 0;
 	int j = 0;
-	for (int y = 0; y < h; y ++ )
+	for (int y = 0; y < h; y++)
 	{
-		i = y * zoom;
-		j=0;
-		for (int x = 0; x < w; x ++)
+
+		for (int x = 0; x < w; x++)
 		{
-			
+			i = y * scaleY;
+			j = x * scaleX;
 			g[i * new_w + j] = img[y * w + x];
-			j=j+zoom;
-		}
-		
-	}
-
-	for ( int y=0; y<new_h;y+=zoom)
-	{
-		for (int x = 1; x<new_w ; x+=zoom)
-		{
-			g[y * new_w + x] = g[y * new_w + (x-1)];
 		}
 	}
-
-	for( int y = 1 ; y<new_h ;y+=zoom)	
+	unsigned char left;
+	unsigned char right;
+	for (int y = 0; y < new_h; y += scaleY)
 	{
-		for(int x=0;x<new_w;x+=zoom)
+		for (int x = 0; x < new_w - scaleX; x += scaleX)
 		{
-			g[y*new_w + x] = g[(y-1) * new_w + x];
+			left = g[y * new_w + x];
+			right = g[y * new_w + (x + scaleX)];
+
+			//from left to right, fill all pixels
+			for(int k =1; k<scaleX;k++)
+			{
+				g[y * new_w + (x + k)] = (left+right) / 2;
+			}
+
+			
+		}
+		// g[y * new_w + (w-1)]  = g[y*new_w + (w-2)]; // copy the penultimate column to last column
+	}
+
+	unsigned char top;
+	unsigned char bottom;
+	for (int y = 0; y < new_h - scaleY; y += scaleY)
+	{
+		for (int x = 0; x < new_w; x ++)
+		{
+			top  = g[y*new_w + x];
+			bottom = g[(y+scaleY) * new_w + x];
+
+			for(int k=1;k<scaleY;k++)
+			{
+				g[(y + k) * new_w + x] = (top+bottom) / 2;
+			}
+			
+		}
+	}
+
+	return g;
+}
+
+unsigned char *nearestNeighborZoomImage(unsigned char *img, int w, int h, int scaleX, int scaleY)
+{
+	int new_w, new_h;
+	new_h = h * scaleY;
+	new_w = w * scaleX;
+	unsigned char *g = new unsigned char[new_w * new_h];
+
+	int src_y;
+	int src_x;
+	for (int y = 0; y < new_h; y++)
+	{
+		src_y = y / scaleY;
+		if (src_y >= h)
+		{
+			src_y = h - 1;
+		}
+		for (int x = 0; x < new_w; x++)
+		{
+			src_x = x / scaleX;
+			if (src_x >= w)
+			{
+				src_x = w - 1;
+			}
+			g[y * new_w + x] = img[src_y * w + src_x];
 		}
 	}
 
