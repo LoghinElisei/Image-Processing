c0471dd67b1a3a0143abcb97748b16229ddd2c3d
LoghinElisei <loghinelisei04@gmail.com>
2025-11-29 21:18:25 +0200

    add gitignore

---
+++ L9/operatii.cpp
@@ -0,0 +1,38 @@
+#include "operatii.h"
+
+
+
+
+cv::Mat drawHistogram(const cv::Mat gray)
+{
+	int channels[] = { 0 };
+	cv::Mat hist;
+	float range[] = { 0,256 };
+	const float* histRange = { range };
+	int histSize = 256;
+	int hist_w = 512;
+	int hist_h = 400;
+	int bin_w = cvRound((double)hist_w / histSize);
+	double maxVal = 0;
+
+	cv::calcHist(&gray, 1, channels, cv::Mat(), hist, 1, &histSize, &histRange, true, false);
+
+	cv::Mat histImage(hist_h, hist_w, CV_8UC3, cv::Scalar(255,255,255));
+
+
+	cv::minMaxLoc(hist, 0, &maxVal);
+	cv::Scalar lineColor(255, 0, 0);
+
+	for (int i = 0; i < histSize; i++)
+	{
+		float value = hist.at<float>(i);
+		int height = cvRound((value / maxVal) * hist_h);
+
+		cv::line(histImage, cv::Point(i * bin_w, hist_h), cv::Point(i * bin_w, hist_h - height), lineColor,1);
+	}
+
+
+
+	return histImage;
+}
+
---
+++ L9/operatii.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+
+cv::Mat drawHistogram(const cv::Mat gray);
+
+class ImageGrid {
+public:
+    std::vector<std::pair<std::string, cv::Mat>> images;
+
+    void add(const std::string& name, const cv::Mat& img) {
+        images.push_back({ name, img });
+    }
+
+    void show(const std::string& windowName) {
+        if (images.empty()) return;
+
+        size_t count = images.size();
+        int rows = (int)std::ceil(std::sqrt(count));
+        int cols = rows;
+
+        int img_h = 300; // dimensiunea fiecărei imagini în grid
+        int img_w = 450;
+       
+
+        cv::Mat grid = cv::Mat::zeros(rows * img_h, cols * img_w, CV_8UC3);
+
+        int idx = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (idx >= count) break;
+
+                cv::Mat resized;
+                cv::Mat input = images[idx].second;
+
+                if (input.channels() == 1)
+                    cv::cvtColor(input, resized, cv::COLOR_GRAY2BGR);
+                else
+                    resized = input.clone();
+
+                cv::resize(resized, resized, cv::Size(img_w, img_h));
+
+                resized.copyTo(grid(cv::Rect(c * img_w, r * img_h, img_w, img_h)));
+
+                cv::putText(grid, images[idx].first,
+                    cv::Point(c * img_w + 10, r * img_h + 20),
+                    cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 0), 1);
+                idx++;
+            }
+        }
+
+        cv::imshow(windowName, grid);
+    }
+};
+
+
